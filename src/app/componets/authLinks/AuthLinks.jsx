180c061f30791d73f26800e1a0d90051e51d80e1
Ashen Shamilka Wickramasinghe <ashenshamilka270@gmail.com>
2024-03-20 12:51:51 +0530

    componets updated

---
+++ src/app/componets/authLinks/AuthLinks.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './AuthLinks.module.css'
+
+
+
+const AuthLinks = () => {
+  return (
+    <div className={styles.container} >authLinks</div>
+  )
+}
+
+export default AuthLinks
--- src/app/componets/navbar/NavBar.jsx
+++ src/app/componets/navbar/NavBar.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
 import styles from './NavBar.module.css'
+import Image from 'next/image'
+import Link from 'next/link'
+import ThemeToggle from '../ThemeToggle/ThemeToggle'
+import AuthLinks from '../authLinks/AuthLinks'
+
+
 
 const NavBar = () => {
   return (
-    <div className={styles.container}>NavBar</div>
+    <div className={styles.container}>
+     <div className={styles.social}>
+         <Image src="/images/facebook.png" alt="facebook" width={24} height={24} />
+          <Image src="/images/instagram.png" alt="instagram" width={24} height={24} />
+          <Image src="/images/tiktok.png" alt="tiktok" width={24} height={24} />
+          <Image src="/images/youtube.png" alt="youtube" width={24} height={24} />
+     </div>
+     <div className={styles.logo}>TechnoBlog</div>
+     <div className={styles.links}>
+           
+          
+           <ThemeToggle/>
+           <Link href="/">Homepage</Link>
+           <Link href="/">Contact</Link>
+           <Link href="/">About</Link>
+           <AuthLinks/>
+          
+
+
+     </div>
+     
+    </div>
   )
 }
 
