349ddf87375364b92c811b55be63829a15eb705d
Ashen Shamilka Wickramasinghe <ashenshamilka270@gmail.com>
2024-03-24 20:44:01 +0530

    completed the write page

--- src/app/Write/page.jsx
+++ src/app/Write/page.jsx
@@ -1,20 +1,20 @@
 "use client";
-
 import React from 'react'
 import styles from './writePage.module.css'
 import Image from 'next/image'
 import { useState } from 'react'
 import ReactQuill from 'react-quill'
 import 'react-quill/dist/quill.bubble.css'
+
   
 const WritePage = () => {
        const [open, setOpen] = useState(false);
        const [value, setValue] = useState(false);
       return (
         <div className={styles.container}>
-           <input type="text" placeholder="Title" className={styles.title}/>
+           <input type="text" placeholder="Title" className={styles.input}/>
               <div className={styles.editor}>
-                 <button className={styles.button}>
+                 <button className={styles.button} onClick={() => setOpen (!open)}>
                    <Image src="/plus.png" alt="" width={16} height={16}/> 
                  </button>
                  {open && (
@@ -23,17 +23,20 @@ const WritePage = () => {
                         <Image src="/image.png" alt="" width={16} height={16}/>
                       </button>
                       <button className={styles.addButton}>
-                        <Image src="/image.png" alt="" width={16} height={16}/>
+                        <Image src="/external.png" alt="" width={16} height={16}/>
                       </button>
                       <button className={styles.addButton}>
-                        <Image src="/image.png" alt="" width={16} height={16}/>
+                        <Image src="/video.png" alt="" width={16} height={16}/>
                       </button>
                     </div>
                  )}
-               <ReactQuill theme="bubble" value={value} onChange={setValue} placeholder="Tell your Story...."/>
-
-
+               <ReactQuill className={styles.textArea}
+               theme="bubble" 
+               value={value} 
+               onChange={setValue} 
+               placeholder="Tell your Story...."/>
               </div>
+              <button className={styles.publish}>Publish</button>
         </div>
       );
     };
